ac66b8fe7c28fb724a4de17fc15eb5f1cee290ca
goldford <gr336681@dal.ca>
2020-10-29 20:41:50 -0700

    Update index.js

--- salish-sea-demo/index.js
+++ salish-sea-demo/index.js
@@ -10,8 +10,8 @@ const gl = canvas.getContext('webgl', {antialiasing: false});
 // go - replacing hardcoded stuff lines 83/84
 const degspan_height = 5;
 const degspan_width = 9;
-const maxlon = 129; //
-const maxlat = 51;
+const minlon = 129; //neg to pos
+const maxlat = 51.4;
 
 const wind = window.wind = new WindGL(gl);
 wind.numParticles = 65536;
@@ -84,7 +84,7 @@ getJSON('https://goldford.github.io/webgl-wind/data/sscoast.json', function (dat
 		
         for (let j = 0; j < line.length; j++) {
 			
-			const x_pos = (line[j][0] + maxlon) * canvas.width / degspan_width;
+			const x_pos = (line[j][0] + minlon) * canvas.width / degspan_width;
 			const y_pos = (-line[j][1] + maxlat) * canvas.height / degspan_height;
 			const test1 = isNaN(parseFloat(x_pos));
 			const test2 = isNaN(parseFloat(y_pos));

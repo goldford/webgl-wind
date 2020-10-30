ffab55e951def1212900633d196f52cec7a63cd2
goldford <gr336681@dal.ca>
2020-10-30 16:46:09 -0700

    Update index.js

--- salish-sea-demo/index.js
+++ salish-sea-demo/index.js
@@ -46,14 +46,14 @@ const windFiles = {
 };
 
 const meta = {
-    'Year': 0,
+    'Year-Month': 0,
 	//'2016-11-20+h': 0,
     'retina resolution': true,
     'github.com/goldford/webgl-wind': function () {
         window.location = 'https://github.com/goldford/webgl-wind';
     }
 };
-gui.add(meta, 'Year', 0, 10, 10).onFinishChange(updateWind);
+gui.add(meta, 'Year-Month', 0, 10, 10).onFinishChange(updateWind);
 //gui.add(meta, '2016-11-20+h', 0, 48, 6).onFinishChange(updateWind);
 //gui.add(meta, '1980-05', 0, 48, 6).onFinishChange(updateWind);
 
